f43f2ca28d7f5bab2e5253292a33f8d1fa17a7da
Alonso Melgar Lopez <alonmar3@gmail.com>
2023-11-12 17:04:20 -0600

    ✅ add test dummy

---
+++ tests/test_evaluation.py
@@ -0,0 +1,6 @@
+def test_dummy():
+    # intialise data of lists.
+    list1 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    list2 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+    assert list1 == list2
