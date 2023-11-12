753c33d394aadbe20fd22f739796596381ca9892
Alonso Melgar Lopez <alonmar3@gmail.com>
2023-11-12 17:12:42 -0600

    🔧 add config files

---
+++ tasks/config.py
@@ -0,0 +1,54 @@
+import logging
+import sys
+from pathlib import Path
+
+PARAMETERS_PATH = "./parameters.json"
+# Directories
+ROOT_DIR = "."
+LOGS_DIR = Path(ROOT_DIR, "logs")
+LOGS_DIR.mkdir(parents=True, exist_ok=True)
+
+
+# Logger
+logging_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "minimal": {"format": "%(message)s"},
+        "detailed": {
+            # pylint: disable=line-too-long
+            "format": "%(levelname)s %(asctime)s [%(name)s:%(filename)s:%(funcName)s:%(lineno)d]\n%(message)s\n"
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "stream": sys.stdout,
+            "formatter": "minimal",
+            "level": logging.DEBUG,
+        },
+        "info": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": Path(LOGS_DIR, "info.log"),
+            "maxBytes": 10485760,  # 1 MB
+            "backupCount": 10,
+            "formatter": "detailed",
+            "level": logging.INFO,
+        },
+        "error": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": Path(LOGS_DIR, "error.log"),
+            "maxBytes": 10485760,  # 1 MB
+            "backupCount": 10,
+            "formatter": "detailed",
+            "level": logging.ERROR,
+        },
+    },
+    "root": {
+        "handlers": ["console", "info"],
+        "level": logging.INFO,
+        "propagate": True,
+    },
+}
+logging.config.dictConfig(logging_config)
+logger = logging.getLogger("my-logger")
